280a747d77422ebc5a11a3fc6650354c9542e779
Marek <35031234+thebestofal@users.noreply.github.com>
2019-11-03 10:50:28 +0100

    Update wordy.rb

--- lib/wordy.rb
+++ lib/wordy.rb
@@ -1,17 +1,57 @@
 class WordProblem
+    attr_reader :string
+  
+    OPERATOR = {
+      'plus' => '+',
+      'minus' => '-',
+      'multiply' => '*',
+      'divided' => '/'
+    }
+  
     def initialize(string)
-        @answer = 0
-        if string.scan(/\w+/).include? 'plus'
-            @answer += Integer(x)
-             string.scan(/-?\d+/) do |x|
-            @answer += Integer(x)
-        else
-            
-        end
+      #require 'pry';binding.pry
+      @string = string
+      @digits = find_digit.split.count
+      #raise ArgumentError if invalid?
     end
-
-    def answer 
-        @answer
+  
+    def answer
+    
+     result = if @digits == 2
+        two_digit
+      elsif @digits == 3
+        three_digit
+        elsif @digits < 2
+          raise ArgumentError, "error" if invalid?
+      end
+      eval(result)
     end
-end
-
+  
+    def find_digit
+      string.gsub(/[^\d\-]/, ' ')
+    end
+  
+    def find_operation
+      operator = %w(plus minus multiply divided)
+      string.split(' ').select do |word|
+        operator.include?(word)
+      end
+    end
+  
+    def two_digit
+      first, second = find_digit.split(' ')
+      operator = OPERATOR[find_operation.first]
+      [first,operator,second].join(' ')
+    end
+  
+    def three_digit
+      first, second, third = find_digit.split(' ')
+      first_operator = OPERATOR[find_operation.first]
+      second_operator = OPERATOR[find_operation[1]]
+      [first,first_operator,second,second_operator,third].join(' ')
+    end
+  
+    def invalid?
+      @digits < 2
+    end
+  end
