dd13853ef45d14dbf6ee37d23a7f73666793f1d3
Marek <35031234+thebestofal@users.noreply.github.com>
2019-11-03 11:09:06 +0100

    matrix added

--- lib/matrix.rb
+++ lib/matrix.rb
@@ -1,3 +1,12 @@
 class Matrix
-
+    attr_reader :rows, :columns
+    def initialize(matrix_str)
+        @rows = []
+        @columns = []
+        matrix_str.split("\n").each_with_index { |row|
+            @rows << row.split(' ').map(&:to_i)
+            
+        }
+        @columns = rows.transpose()
+    end
 end
